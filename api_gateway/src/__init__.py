c572806597db5cf2f70c9e3aa23e6114a1854c20
vladyslav-fenchak <vladyslav.fenchak@clariteia.com>
2021-06-15 18:14:07 +0200

    API Gateway initial structure
    Add API Gateway #10
    api gateway routes #7

---
+++ api_gateway/src/__init__.py
@@ -0,0 +1,4 @@
+from .controllers import (
+    Order,
+    Product,
+)
---
+++ api_gateway/src/controllers/order.py
@@ -0,0 +1,24 @@
+from minos.api_gateway.rest import (
+    MicroserviceCallCoordinator,
+)
+from minos.api_gateway.common import (
+    MinosConfig,
+)
+from aiohttp import web
+
+
+class Order:
+    async def add(self, request: web.Request, config: MinosConfig, **kwargs):
+        coordinator = MicroserviceCallCoordinator(config, request, request.url.host, request.url.port)
+        response = await coordinator.orchestrate()
+        return response
+
+    async def get(self, request: web.Request, config: MinosConfig, **kwargs):
+        coordinator = MicroserviceCallCoordinator(config, request, request.url.host, request.url.port)
+        response = await coordinator.orchestrate()
+        return response
+
+    async def history(self, request: web.Request, config: MinosConfig, **kwargs):
+        coordinator = MicroserviceCallCoordinator(config, request, request.url.host, request.url.port)
+        response = await coordinator.orchestrate()
+        return response
