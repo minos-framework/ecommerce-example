e724092ca31a1d1511c73f7cf6bb7d909d71fa24
Restyled.io <commits@restyled.io>
2021-06-21 13:20:30 +0000

    Restyled by black

--- integration_tests/services/payment_provider/config.py
+++ integration_tests/services/payment_provider/config.py
@@ -6,12 +6,20 @@ This file is part of minos framework.
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
 from minos.common import PostgreSqlMinosRepository
-from minos.networks import (CommandBroker, CommandConsumerService,
-                            CommandHandlerService, CommandReplyBroker,
-                            CommandReplyConsumerService,
-                            CommandReplyHandlerService, EventBroker,
-                            EventConsumerService, EventHandlerService,
-                            ProducerService, RestService, SnapshotService)
+from minos.networks import (
+    CommandBroker,
+    CommandConsumerService,
+    CommandHandlerService,
+    CommandReplyBroker,
+    CommandReplyConsumerService,
+    CommandReplyHandlerService,
+    EventBroker,
+    EventConsumerService,
+    EventHandlerService,
+    ProducerService,
+    RestService,
+    SnapshotService,
+)
 from minos.saga import SagaManager
 
 injections = {
