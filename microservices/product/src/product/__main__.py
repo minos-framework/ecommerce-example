fc3087f3ff9dcb64e9d8c18c4500e1c5e261764e
Restyled.io <commits@restyled.io>
2021-06-21 13:00:55 +0000

    Restyled by black

--- microservices/product/src/product/__main__.py
+++ microservices/product/src/product/__main__.py
@@ -5,9 +5,7 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from .cli import (
-    main,
-)
+from .cli import main
 
 
 if __name__ == "__main__":
