80a4b92d67c546fc3d0ca5ab5bdff65d5a9408d7
Restyled.io <commits@restyled.io>
2021-06-21 15:43:41 +0000

    Restyled by black

--- microservices/product/src/cli.py
+++ microservices/product/src/cli.py
@@ -5,12 +5,8 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from pathlib import (
-    Path,
-)
-from typing import (
-    Optional,
-)
+from pathlib import Path
+from typing import Optional
 
 import typer
 
