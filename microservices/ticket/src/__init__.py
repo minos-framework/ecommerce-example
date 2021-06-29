7933c0d8d12f46a2dddf3b0bfb3f6e4a4869c8ba
Restyled.io <commits@restyled.io>
2021-06-29 06:32:02 +0000

    Restyled by black

--- microservices/ticket/src/__init__.py
+++ microservices/ticket/src/__init__.py
@@ -5,15 +5,7 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from .aggregates import (
-    Ticket,
-)
-from .controllers import (
-    TicketController,
-)
-from .services import (
-    TicketService,
-)
-from .sagas import (
-    _CREATE_TICKET
-)
+from .aggregates import Ticket
+from .controllers import TicketController
+from .services import TicketService
+from .sagas import _CREATE_TICKET
--- microservices/ticket/src/services.py
+++ microservices/ticket/src/services.py
@@ -5,14 +5,10 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from minos.common import (
-    Service,
-)
+from minos.common import Service
 from minos.saga import SagaContext
 
-from .aggregates import (
-    Ticket,
-)
+from .aggregates import Ticket
 
 import uuid
 
