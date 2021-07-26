5313efac0d3abb46719939537443f7d2170fda0b
Restyled.io <commits@restyled.io>
2021-07-26 18:41:37 +0000

    Restyled by black

--- microservices/order/src/queries/services.py
+++ microservices/order/src/queries/services.py
@@ -6,9 +6,7 @@ This file is part of minos framework.
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
 
-from minos.cqrs import (
-    QueryService,
-)
+from minos.cqrs import QueryService
 from minos.networks import (
     enroute,
     Request,
