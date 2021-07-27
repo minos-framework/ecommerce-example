c66996b89ce76ed3e5d0b8f95fdc3cd51306fec3
Sergio García Prado <sergio.garcia@clariteia.com>
2021-07-27 10:49:36 +0200

    ISSUE #116 * Add docstring.

--- microservices/order/src/queries/services.py
+++ microservices/order/src/queries/services.py
@@ -16,31 +16,31 @@ from minos.networks import (
 
 
 class OrderQueryService(QueryService):
-    """TODO"""
+    """Order Query Service class."""
 
     @enroute.broker.event("OrderCreated")
     async def order_created(self, request: Request):
-        """TODO
+        """Handle the order creation events.
 
-        :param request: TODO
-        :return: TODO
+        :param request: A request instance containing the aggregate difference.
+        :return: This method does not return anything.
         """
         print(await request.content())
 
     @enroute.broker.event("OrderUpdated")
     async def order_updated(self, request: Request):
-        """TODO
+        """Handle the order update events.
 
-        :param request: TODO
-        :return: TODO
+        :param request: A request instance containing the aggregate difference.
+        :return: This method does not return anything.
         """
         print(await request.content())
 
     @enroute.broker.event("OrderDeleted")
     async def order_deleted(self, request: Request):
-        """TODO
+        """Handle the order deletion events.
 
-        :param request: TODO
-        :return: TODO
+        :param request: A request instance containing the aggregate difference.
+        :return: This method does not return anything.
         """
         print(await request.content())
