73682a892ae1d37e4b6c107a15b3e219fcd19336
Restyled.io <commits@restyled.io>
2021-07-28 07:46:03 +0000

    Restyled by isort

--- microservices/payment/src/queries/services.py
+++ microservices/payment/src/queries/services.py
@@ -5,9 +5,13 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from typing import NoReturn
+from typing import (
+    NoReturn,
+)
 
-from minos.cqrs import QueryService
+from minos.cqrs import (
+    QueryService,
+)
 from minos.networks import (
     Request,
     enroute,
--- microservices/payment/tests/test_commands/test_services.py
+++ microservices/payment/tests/test_commands/test_services.py
@@ -5,14 +5,24 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from __future__ import annotations
+from __future__ import (
+    annotations,
+)
 
 import sys
 import unittest
-from asyncio import gather
-from pathlib import Path
-from typing import NoReturn
-from uuid import UUID
+from asyncio import (
+    gather,
+)
+from pathlib import (
+    Path,
+)
+from typing import (
+    NoReturn,
+)
+from uuid import (
+    UUID,
+)
 
 from minos.common import (
     CommandReply,
