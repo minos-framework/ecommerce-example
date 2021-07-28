4ae7d743c98123c0ae141fc5a4e644c800633d3c
Alberto Amigo Alonso <albamigh@gmail.com>
2021-07-28 09:56:21 +0200

    issue #124
    - Added test endpoint.

--- api_gateway/src/__init__.py
+++ api_gateway/src/__init__.py
@@ -1,1 +1,2 @@
 from .login_controller import LoginController
+
--- api_gateway/src/login_controller.py
+++ api_gateway/src/login_controller.py
@@ -3,10 +3,20 @@ from minos.api_gateway.common import (
     MinosConfig,
 )
 import jwt
+from jwt.exceptions import InvalidSignatureError
+
+SECRET = "secret"
 
 
 class LoginController:
-    async def login(self, request: web.Request, config: MinosConfig):
-        encoded_jwt = jwt.encode({"some": "payload"}, "secret", algorithm="HS256")
-        print(encoded_jwt)
-        print(jwt.decode(encoded_jwt, "secret", algorithms=["HS256"]))
+    async def login(self, request: web.Request, config: MinosConfig) -> web.Response:
+        token = jwt.encode({"some": "payload"}, SECRET, algorithm="HS256")
+        return web.Response(text=encoded_jwt)
+
+    async def test_endpoint(self, request: web.Request, config: MinosConfig) -> web.Response:
+        token = request.headers['Authorization'].split()[-1]
+        try:
+            msg = jwt.decode(token, SECRET, algorithms=["HS256"])
+        except InvalidSignatureError as e:
+            msg = e
+        return web.Response(text=str(msg))
