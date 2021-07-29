1e7bcd031763eb49a30a98a89e3e3cbf67172b29
Vladyslav Fenchak <vladyslav.fenchak@clariteia.com>
2021-07-29 20:42:06 +0200

    Move logic to repository
    Implement the TicketQueryService functionality #147

---
+++ microservices/ticket/src/queries/repositories.py
@@ -0,0 +1,58 @@
+"""
+Copyright (C) 2021 Clariteia SL
+This file is part of minos framework.
+Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
+"""
+from __future__ import annotations
+from typing import NoReturn
+from uuid import UUID
+
+from minos.common import MinosConfig, PostgreSqlMinosDatabase
+
+
+class TicketAmountRepository(PostgreSqlMinosDatabase):
+    """Ticket Amount repository"""
+
+    async def _setup(self) -> NoReturn:
+        await self.submit_query(_CREATE_TABLE)
+
+    @classmethod
+    def _from_config(cls, *args, config: MinosConfig, **kwargs) -> TicketAmountRepository:
+        return cls(database="ticket_query_db", port=5432, user="minos", password="min0s", host="localhost")
+
+    async def insert_ticket_amount(self, uuid: UUID, total_price: int) -> NoReturn:
+        """ Insert Payment amount
+        :param uuid: UUID
+        :param total_price: Amount in float format
+        :return: Nothing
+        """
+        await self.submit_query(_INSERT_TICKET_QUERY, {"uuid": uuid, "total_price": total_price})
+
+    async def delete(self, uuid: UUID) -> NoReturn:
+        """ Delete Payment
+        :param uuid: UUID
+        :return: Nothing
+        """
+        await self.submit_query(_DELETE_TICKET_QUERY, {"uuid": uuid})
+
+
+_CREATE_TABLE = """
+CREATE TABLE IF NOT EXISTS ticket (
+    uuid UUID NOT NULL PRIMARY KEY,
+    total_price FLOAT NOT NULL
+);
+""".strip()
+
+_INSERT_TICKET_QUERY = """
+INSERT INTO ticket (uuid, total_price)
+VALUES (%(uuid)s,  %(total_price)s)
+ON CONFLICT (uuid)
+DO
+   UPDATE SET total_price = %(total_price)s
+;
+""".strip()
+
+_DELETE_TICKET_QUERY = """
+DELETE FROM ticket
+WHERE uuid = %(uuid)s;
+""".strip()
--- microservices/ticket/src/queries/services.py
+++ microservices/ticket/src/queries/services.py
@@ -21,6 +21,8 @@ from minos.networks import (
     enroute,
 )
 
+from src.queries.repositories import TicketAmountRepository
+
 
 class TicketQueryService(QueryService):
     """Ticket Query Service class."""
@@ -32,18 +34,12 @@ class TicketQueryService(QueryService):
         :param request: A request instance containing the aggregate difference.
         :return: This method does not return anything.
         """
-        """Handle the product create and update events.
-        :param request: A request instance containing the aggregate difference.
-        :return: This method does not return anything.
-        """
         diff: AggregateDiff = await request.content()
         uuid = diff.uuid
         total_price = diff.fields_diff["total_price"]
 
-        async with await self._get_connection() as connection:
-            await self._create_table(connection)
-            async with connection.cursor() as cursor:
-                await cursor.execute(_INSERT_TICKET_QUERY, {"uuid": uuid, "total_price": total_price})
+        async with TicketAmountRepository.from_config(config=self.config) as repository:
+            await repository.insert_ticket_amount(uuid, total_price)
 
     @enroute.broker.event("TicketDeleted")
     async def ticket_deleted(self, request: Request) -> NoReturn:
@@ -53,37 +49,9 @@ class TicketQueryService(QueryService):
         """
         diff: AggregateDiff = await request.content()
 
-        async with await self._get_connection() as connection:
-            await self._create_table(connection)
-            async with connection.cursor() as cursor:
-                await cursor.execute(_DELETE_TICKET_QUERY, {"uuid": diff.uuid})
-
-    @staticmethod
-    async def _get_connection():
-        return await aiopg.connect(database="ticket_query_db", user="minos", password="min0s", host="localhost")
-
-    async def _create_table(self, connection):
-        async with connection.cursor() as cursor:
-            await cursor.execute(_CREATE_TABLE)
+        async with TicketAmountRepository.from_config(config=self.config) as repository:
+            await repository.delete(diff.uuid)
 
 
-_CREATE_TABLE = """
-CREATE TABLE IF NOT EXISTS ticket (
-    uuid UUID NOT NULL PRIMARY KEY,
-    total_price FLOAT NOT NULL
-);
-""".strip()
 
-_INSERT_TICKET_QUERY = """
-INSERT INTO ticket (uuid, total_price)
-VALUES (%(uuid)s,  %(total_price)s)
-ON CONFLICT (uuid)
-DO
-   UPDATE SET total_price = %(total_price)s
-;
-""".strip()
 
-_DELETE_TICKET_QUERY = """
-DELETE FROM ticket
-WHERE uuid = %(uuid)s;
-""".strip()
