db305bc48d10a0ad375a0b8d5d7645c6f044fc7f
Vladyslav Fenchak <vladyslav.fenchak@clariteia.com>
2021-07-29 14:46:38 +0200

    Implement the PaymentQueryService functionality
    Implement the PaymentQueryService functionality #153

--- microservices/payment/src/queries/services.py
+++ microservices/payment/src/queries/services.py
@@ -8,7 +8,9 @@ Minos framework can not be copied and/or distributed without the express permiss
 from typing import (
     NoReturn,
 )
-
+from minos.common import (
+    AggregateDiff,
+)
 from minos.cqrs import (
     QueryService,
 )
@@ -16,37 +18,69 @@ from minos.networks import (
     Request,
     enroute,
 )
+import aiopg
 
 
 class PaymentQueryService(QueryService):
     """Payment Query Service class"""
 
-    @staticmethod
     @enroute.broker.event("PaymentCreated")
-    async def payment_created(request: Request) -> NoReturn:
+    @enroute.broker.event("PaymentUpdated")
+    async def payment_created_or_updated(self, request: Request) -> NoReturn:
         """Handle the payment create events.
 
         :param request: A request instance containing the aggregate difference.
         :return: This method does not return anything.
         """
-        print(await request.content())
+        diff: AggregateDiff = await request.content()
+        uuid = diff.uuid
+        amount = diff.fields_diff["amount"]
 
-    @staticmethod
-    @enroute.broker.event("PaymentUpdated")
-    async def payment_updated(request: Request) -> NoReturn:
-        """Handle the payment update events.
+        async with await self._get_connection() as connection:
+            await self._create_table(connection)
+            async with connection.cursor() as cursor:
+                await cursor.execute(_INSERT_PAYMENT_QUERY, {"uuid": uuid, "amount": amount})
 
-        :param request: A request instance containing the aggregate difference.
-        :return: This method does not return anything.
-        """
-        print(await request.content())
-
-    @staticmethod
     @enroute.broker.event("PaymentDeleted")
-    async def payment_deleted(request: Request) -> NoReturn:
+    async def payment_deleted(self, request: Request) -> NoReturn:
         """Handle the payment delete events.
 
         :param request: A request instance containing the aggregate difference.
         :return: This method does not return anything.
         """
-        print(await request.content())
+        diff: AggregateDiff = await request.content()
+
+        async with await self._get_connection() as connection:
+            await self._create_table(connection)
+            async with connection.cursor() as cursor:
+                await cursor.execute(_DELETE_PAYMENT_QUERY, {"uuid": diff.uuid})
+
+    @staticmethod
+    async def _get_connection():
+        return await aiopg.connect(database="payment_query_db", user="minos", password="min0s", host="localhost")
+
+    async def _create_table(self, connection):
+        async with connection.cursor() as cursor:
+            await cursor.execute(_CREATE_TABLE)
+
+
+_CREATE_TABLE = """
+CREATE TABLE IF NOT EXISTS payment (
+    uuid UUID NOT NULL PRIMARY KEY,
+    amount FLOAT NOT NULL
+);
+""".strip()
+
+_INSERT_PAYMENT_QUERY = """
+INSERT INTO payment (uuid, amount)
+VALUES (%(uuid)s,  %(amount)s)
+ON CONFLICT (uuid)
+DO
+   UPDATE SET amount = %(amount)s
+;
+""".strip()
+
+_DELETE_PAYMENT_QUERY = """
+DELETE FROM payment
+WHERE uuid = %(uuid)s;
+""".strip()
