4277ea4e33326bdcb09b945e858e39c33905f0e2
Restyled.io <commits@restyled.io>
2021-07-29 14:38:20 +0000

    Restyled by isort

--- microservices/product/src/queries/services.py
+++ microservices/product/src/queries/services.py
@@ -5,10 +5,16 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from typing import NoReturn
+from typing import (
+    NoReturn,
+)
 
-from minos.common import AggregateDiff
-from minos.cqrs import QueryService
+from minos.common import (
+    AggregateDiff,
+)
+from minos.cqrs import (
+    QueryService,
+)
 from minos.networks import (
     Request,
     Response,
@@ -16,7 +22,9 @@ from minos.networks import (
     enroute,
 )
 
-from .repositories import ProductInventoryRepository
+from .repositories import (
+    ProductInventoryRepository,
+)
 
 
 class ProductQueryService(QueryService):
