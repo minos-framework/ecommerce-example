81177cfae64a1ecfad10f5efdc0d2f0298dc3c08
Alberto Amigo Alonso <albamigh@gmail.com>
2021-07-29 12:22:51 +0200

    issue #143
    - Added password to User.

--- microservices/user/src/queries/__init__.py
+++ microservices/user/src/queries/__init__.py
@@ -5,14 +5,6 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from .aggregates import (
-    Address,
-    User,
-)
-from .commands import (
-    UserCommandService,
-)
-
-from .queries import (
+from .services import (
     UserQueryService,
 )
