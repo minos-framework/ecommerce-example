77a35d650014b548bddcc2a725ea94472f93f0ad
Restyled.io <commits@restyled.io>
2021-07-30 12:01:45 +0000

    Restyled by black

--- microservices/ticket/src/__init__.py
+++ microservices/ticket/src/__init__.py
@@ -5,9 +5,7 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from .aggregates import (
-    Ticket,
-)
+from .aggregates import Ticket
 from .commands import (
     _CREATE_TICKET,
     TicketCommandService,
