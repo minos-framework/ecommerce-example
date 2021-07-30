fdbd1bedf6400fcbc6d25a684933d5bc6d3d3193
Restyled.io <commits@restyled.io>
2021-07-30 10:34:38 +0000

    Restyled by isort

--- microservices/payment/src/__init__.py
+++ microservices/payment/src/__init__.py
@@ -5,9 +5,13 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from .aggregates import Payment
-from .commands import PaymentCommandService
+from .aggregates import (
+    Payment,
+)
+from .commands import (
+    PaymentCommandService,
+)
 from .queries import (
-    PaymentQueryService,
     PaymentAmountRepository,
+    PaymentQueryService,
 )
--- microservices/payment/src/queries/services.py
+++ microservices/payment/src/queries/services.py
@@ -5,16 +5,26 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from typing import NoReturn
+from typing import (
+    NoReturn,
+)
 
-from minos.common import AggregateDiff
-from minos.cqrs import QueryService
+from dependency_injector.wiring import (
+    Provide,
+)
+from minos.common import (
+    AggregateDiff,
+)
+from minos.cqrs import (
+    QueryService,
+)
 from minos.networks import (
     Request,
     enroute,
 )
-from src.queries.repositories import PaymentAmountRepository
-from dependency_injector.wiring import Provide
+from src.queries.repositories import (
+    PaymentAmountRepository,
+)
 
 
 class PaymentQueryService(QueryService):
