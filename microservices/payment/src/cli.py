913f17a8b66c760a08a0b44c65fd7d5aa10e9f1b
Sergio García Prado <sergio.garcia@clariteia.com>
2021-08-03 15:31:59 +0200

    ISSUE #153 * Simplify CLI.

--- microservices/payment/src/cli.py
+++ microservices/payment/src/cli.py
@@ -6,6 +6,7 @@ This file is part of minos framework.
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
 import logging
+import sys
 from pathlib import (
     Path,
 )
@@ -24,13 +25,6 @@ logging.getLogger("aiohttp.access").setLevel(logging.WARNING)
 app = typer.Typer()
 
 
-class _MyEntrypointLauncher(EntrypointLauncher):
-    async def _setup(self):
-        import src
-
-        await self.injector.wire(modules=[src] + self._internal_modules)
-
-
 @app.command("start")
 def start(
     file_path: Optional[Path] = typer.Argument(
@@ -39,7 +33,7 @@ def start(
 ):
     """Start the microservice."""
     config = MinosConfig(file_path)
-    launcher = _MyEntrypointLauncher.from_config(config=config)
+    launcher = EntrypointLauncher.from_config(config=config, external_modules=[sys.modules["src"]])
     launcher.launch()
 
 
