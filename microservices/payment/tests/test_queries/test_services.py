b6f450ca8fc88b2b1df47b7a0c1033b9a3f89d83
Restyled.io <commits@restyled.io>
2021-08-04 10:51:44 +0000

    Restyled by black

--- microservices/payment/tests/test_queries/test_services.py
+++ microservices/payment/tests/test_queries/test_services.py
@@ -5,18 +5,12 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from __future__ import (
-    annotations,
-)
+from __future__ import annotations
 
 import sys
 import unittest
-from asyncio import (
-    gather,
-)
-from pathlib import (
-    Path,
-)
+from asyncio import gather
+from pathlib import Path
 from typing import (
     NoReturn,
     Optional,
@@ -26,9 +20,7 @@ from uuid import (
     uuid4,
 )
 
-from cached_property import (
-    cached_property,
-)
+from cached_property import cached_property
 from minos.common import (
     CommandReply,
     DependencyInjector,
@@ -39,9 +31,7 @@ from minos.common import (
     MinosSagaManager,
     Model,
 )
-from minos.networks import (
-    Request,
-)
+from minos.networks import Request
 from src import (
     Payment,
     PaymentQueryService,
--- microservices/product/src/queries/__init__.py
+++ microservices/product/src/queries/__init__.py
@@ -5,9 +5,5 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from .repositories import (
-    ProductQueryRepository,
-)
-from .services import (
-    ProductQueryService,
-)
+from .repositories import ProductQueryRepository
+from .services import ProductQueryService
