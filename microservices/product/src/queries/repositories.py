dac5e30bc60d4f07d4de49590022beaa036ccf99
Sergio García Prado <sergio.garcia@clariteia.com>
2021-08-05 11:19:44 +0200

    ISSUE #168 * Revert to `psycopg2`

--- microservices/product/src/queries/repositories.py
+++ microservices/product/src/queries/repositories.py
@@ -11,22 +11,14 @@ from __future__ import (
 
 from typing import (
     NoReturn,
-    Optional,
 )
 from uuid import (
     UUID,
 )
 
-from aiopg.sa import (
-    SAConnection,
-)
-from aiopg.sa.engine import (
-    get_dialect,
-)
 from minos.common import (
     MinosConfig,
-    PostgreSqlMinosDatabase,
-    PostgreSqlPool,
+    MinosSetup,
 )
 from sqlalchemy import (
     create_engine,
@@ -39,18 +31,15 @@ from .models import (
 )
 
 
-class ProductQueryRepository(PostgreSqlMinosDatabase):
+class ProductQueryRepository(MinosSetup):
     """ProductInventory Repository class."""
-
-    _pool: Optional[PostgreSqlPool] = None
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.engine = create_engine("postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}".format(**kwargs))
 
     async def _setup(self) -> NoReturn:
-        await super()._setup()
-        url = f"postgresql+psycopg2://{self.user}:{self.password}@{self.host}:{self.port}/{self.database}"
-        META.create_all(create_engine(url))
+        META.create_all(self.engine)
 
     @classmethod
     def _from_config(cls, *args, config: MinosConfig, **kwargs) -> ProductQueryRepository:
@@ -62,8 +51,8 @@ class ProductQueryRepository(PostgreSqlMinosDatabase):
         :return: a list of dto instances.
         """
         query = PRODUCT_TABLE.select().where(PRODUCT_TABLE.columns.inventory_amount == 0)
-        result = await self._execute(query)
-        return [ProductDTO(**row) async for row in result]
+        result = self.engine.execute(query)
+        return [ProductDTO(**row) for row in result]
 
     async def create(self, **kwargs) -> NoReturn:
         """Create a new row.
@@ -74,7 +63,7 @@ class ProductQueryRepository(PostgreSqlMinosDatabase):
         kwargs["inventory_amount"] = kwargs.pop("inventory")["amount"]
 
         query = PRODUCT_TABLE.insert().values(**kwargs)
-        await self._execute(query)
+        self.engine.execute(query)
 
     async def update(self, uuid: UUID, **kwargs) -> NoReturn:
         """Update an existing row.
@@ -87,7 +76,7 @@ class ProductQueryRepository(PostgreSqlMinosDatabase):
             kwargs["inventory_amount"] = kwargs.pop("inventory")["amount"]
 
         query = PRODUCT_TABLE.update().where(PRODUCT_TABLE.columns.uuid == uuid).values(**kwargs)
-        await self._execute(query)
+        self.engine.execute(query)
 
     async def delete(self, uuid: UUID) -> NoReturn:
         """Delete an entry from the database.
@@ -96,18 +85,4 @@ class ProductQueryRepository(PostgreSqlMinosDatabase):
         :return: This method does not return anything.
         """
         query = PRODUCT_TABLE.delete().where(PRODUCT_TABLE.columns.uuid == uuid)
-        await self._execute(query)
-
-    async def _execute(self, query):
-        async with self.pool.acquire() as conn:
-            return await SAConnection(conn, _ENGINE).execute(query)
-
-
-class _Engine:
-    dialect = get_dialect()
-
-    async def release(self, *args, **kwargs):
-        """TODO"""
-
-
-_ENGINE = _Engine()
+        self.engine.execute(query)
