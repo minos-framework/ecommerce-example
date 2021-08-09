2989b3b55639c97b5048b746048e03add41feaa3
Restyled.io <commits@restyled.io>
2021-08-09 14:48:42 +0000

    Restyled by isort

--- microservices/product/tests/test_commands/test_services.py
+++ microservices/product/tests/test_commands/test_services.py
@@ -5,11 +5,15 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from __future__ import annotations
+from __future__ import (
+    annotations,
+)
 
 import sys
 import unittest
-from pathlib import Path
+from pathlib import (
+    Path,
+)
 from typing import (
     NoReturn,
     Optional,
@@ -19,7 +23,9 @@ from uuid import (
     uuid4,
 )
 
-from cached_property import cached_property
+from cached_property import (
+    cached_property,
+)
 from minos.common import (
     CommandReply,
     DependencyInjector,
