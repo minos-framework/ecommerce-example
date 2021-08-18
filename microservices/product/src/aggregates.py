483beef98c79e18ffb815c200bce727978a49798
Restyled.io <commits@restyled.io>
2021-08-18 06:43:39 +0000

    Restyled by isort

--- microservices/product/src/aggregates.py
+++ microservices/product/src/aggregates.py
@@ -5,9 +5,13 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from __future__ import annotations
+from __future__ import (
+    annotations,
+)
 
-from typing import NoReturn
+from typing import (
+    NoReturn,
+)
 
 from minos.common import (
     Aggregate,
