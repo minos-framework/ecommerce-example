d06f32022854727d6b7f052edc76b6f3221fd21c
Sergio García Prado <sergio.garcia@clariteia.com>
2021-08-18 09:00:01 +0200

    ISSUE #183 * Reorder methods

--- microservices/product/src/aggregates.py
+++ microservices/product/src/aggregates.py
@@ -20,38 +20,6 @@ from minos.common import (
 )
 
 
-class Inventory(ValueObject):
-    """Inventory Object Value class."""
-
-    amount: int
-    reserved: int
-    sold: int
-
-    @staticmethod
-    def empty() -> Inventory:
-        """Create an empty inventory.
-
-        :return: An ``Inventory`` instance.
-        """
-        return Inventory(amount=0, reserved=0, sold=0)
-
-    def set_amount(self, amount: int) -> Inventory:
-        """Create a new inventory with updated amount.
-
-        :param amount: The new inventory amount.
-        :return: An ``Inventory`` instance.
-        """
-        return Inventory(amount, self.reserved, self.sold)
-
-    def update_amount(self, amount_diff: int) -> Inventory:
-        """Create a new inventory with updated amount.
-
-        :param amount_diff: The difference from the actual amount.
-        :return: An ``Inventory`` instance.
-        """
-        return Inventory(self.amount + amount_diff, self.reserved, self.sold)
-
-
 class Product(Aggregate):
     """Product class."""
 
@@ -126,3 +94,35 @@ class Product(Aggregate):
         if not feasible:
             await cls.purchase({k: -v for k, v in quantities.items()})
             raise ValueError("The purchase products query could not be satisfied.")
+
+
+class Inventory(ValueObject):
+    """Inventory Object Value class."""
+
+    amount: int
+    reserved: int
+    sold: int
+
+    @staticmethod
+    def empty() -> Inventory:
+        """Create an empty inventory.
+
+        :return: An ``Inventory`` instance.
+        """
+        return Inventory(amount=0, reserved=0, sold=0)
+
+    def set_amount(self, amount: int) -> Inventory:
+        """Create a new inventory with updated amount.
+
+        :param amount: The new inventory amount.
+        :return: An ``Inventory`` instance.
+        """
+        return Inventory(amount, self.reserved, self.sold)
+
+    def update_amount(self, amount_diff: int) -> Inventory:
+        """Create a new inventory with updated amount.
+
+        :param amount_diff: The difference from the actual amount.
+        :return: An ``Inventory`` instance.
+        """
+        return Inventory(self.amount + amount_diff, self.reserved, self.sold)
