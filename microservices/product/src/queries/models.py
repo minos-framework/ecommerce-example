cb6a6dbb0eaf0ec5eaf6485f545bfad702f12396
Restyled.io <commits@restyled.io>
2021-08-24 11:00:07 +0000

    Restyled by black

--- microservices/product/src/queries/models.py
+++ microservices/product/src/queries/models.py
@@ -5,13 +5,9 @@ This file is part of minos framework.
 
 Minos framework can not be copied and/or distributed without the express permission of Clariteia SL.
 """
-from uuid import (
-    UUID,
-)
+from uuid import UUID
 
-from minos.common import (
-    ModelType,
-)
+from minos.common import ModelType
 from sqlalchemy import (
     Column,
     Integer,
@@ -40,6 +36,14 @@ PRODUCT_TABLE = Table(
 )
 
 ProductDTO = ModelType.build(
-    "ProductDTO", {"uuid": UUID, "code": str, "title": str, "description": str, "price": float, "reviews_count": int,
-                   "reviews_score": float}
+    "ProductDTO",
+    {
+        "uuid": UUID,
+        "code": str,
+        "title": str,
+        "description": str,
+        "price": float,
+        "reviews_count": int,
+        "reviews_score": float,
+    },
 )
