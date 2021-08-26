481fb7e54369815d7fcfdf382de5030b0544f2f4
Vladyslav Fenchak <vladyslav.fenchak@clariteia.com>
2021-08-26 20:58:12 +0200

    ISSUE #238 - Add items to Cart

--- external/front/src/components/products/product-item.js
+++ external/front/src/components/products/product-item.js
@@ -1,59 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button, Card, Col} from "react-bootstrap";
 import {RatingView} from 'react-simple-star-rating'
 import StyledLink from '../shared/styled-link'
-import { CartProvider, useCart } from "react-use-cart";
+import { useCart } from "react-use-cart";
 
-class ProductCard extends React.Component {
+function ProductCard(props) {
+    const [product] = useState({...props});
 
-    constructor(props) {
-        super(props);
-        this.state = {}
-    }
+    const { addItem } = useCart();
 
-    componentDidMount() {
-        this.setState({
-            ...this.props,
-        });
-    }
-
-    componentWillReceiveProps(nextProps) {
-        this.setState({
-            product: nextProps.product,
-        });
-    }
-
-    render() {
-        const {id, title, description, price, reviews_count, reviews_score} = this.state
-
-
-
-        return (
+    return (
             <Col>
                 <Card style={{width: '18rem'}} className="mt-3">
-                    <StyledLink to={"/product/" + id}>
+                    <StyledLink to={"/product/" + product.id}>
 
                     <Card.Img variant="top"
                               src="https://knowledge.insead.edu/sites/www.insead.edu/files/styles/w_650/public/styles/panoramic/public/images/2014/02/coke.jpg?itok=nMcR-Ore"/>
                     <Card.Body className="pl-0 pr-0 pb-2">
-                        <Card.Title>{title}</Card.Title>
-                        <RatingView ratingValue={reviews_score}/>
-                        <span className="ml-2 align-top">{reviews_count}</span>
+                        <Card.Title>{product.title}</Card.Title>
+                        <RatingView ratingValue={product.reviews_score}/>
+                        <span className="ml-2 align-top">{product.reviews_count}</span>
                         <Card.Text>
                             <h5>
-                                <span className="font-weight-bold">{price}</span>
+                                <span className="font-weight-bold">{product.price}</span>
                                 <small className="ml-2 align-top">€</small>
                             </h5>
 
-                            {description}
+                            {product.description}
                         </Card.Text>
                     </Card.Body>
                         </StyledLink>
-                    <Button onClick={() => useCart().addItem(this.state)} variant="add-to-cart" size="lg">Add to cart</Button>
+                    <Button onClick={() => addItem(product)} variant="add-to-cart" size="lg">Add to cart</Button>
                 </Card>
             </Col>
         );
-    }
 }
 
 export default ProductCard;
