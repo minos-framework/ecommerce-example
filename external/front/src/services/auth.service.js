12617ed85d9c8e5903341732913bbd30a6b650b5
Vladyslav Fenchak <vladyslav.fenchak@clariteia.com>
2021-08-26 12:31:58 +0200

    ISSUE #214 - Register User

--- external/front/src/services/auth.service.js
+++ external/front/src/services/auth.service.js
@@ -1,38 +1,70 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/auth/";
+const API_URL = "http://localhost:5566/";
 
 class AuthService {
-  login(username, password) {
-    return axios
-      .post(API_URL + "signin", {
-        username,
-        password
-      })
-      .then(response => {
-        if (response.data.accessToken) {
-          localStorage.setItem("user", JSON.stringify(response.data));
-        }
+    login(username, password) {
+        return axios
+            .post(API_URL + "login", {
+                "username": username,
+                "password": password
+            })
+            .then(response => {
+                if (response.data.accessToken) {
+                    localStorage.setItem("user", JSON.stringify(response.data));
+                }
 
-        return response.data;
-      });
-  }
+                return response.data;
+            });
+    }
 
-  logout() {
-    localStorage.removeItem("user");
-  }
+    logout() {
+        localStorage.removeItem("user");
+    }
 
-  register(username, email, password) {
-    return axios.post(API_URL + "signup", {
-      username,
-      email,
-      password
-    });
-  }
+    register(username, password, street, number) {
+        return axios.post(API_URL + "users", {
+            username: username,
+            password: password,
+            status: "created",
+            address: {
+                street: "Green Dolphin Street",
+                street_no: "42"
+            }
+        }, {
+            headers: {
+                // Overwrite Axios's automatically set Content-Type
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            if (response.data.uuid) {
+                localStorage.setItem("user_uuid", response.data.uuid);
+                localStorage.setItem("user_uuid", response.data.username);
+            }
 
-  getCurrentUser() {
-    return JSON.parse(localStorage.getItem('user'));;
-  }
+            return this.createLogin(username, password);
+        });
+    }
+
+    createLogin(username, password) {
+        return axios.post(API_URL + "login", {
+            username: username,
+            password: password,
+        }, {
+          headers: {
+                // Overwrite Axios's automatically set Content-Type
+                'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            console.log(response.data)
+
+            return response.data;
+        });
+    }
+
+    getCurrentUser() {
+        return JSON.parse(localStorage.getItem('user'));
+    }
 }
 
 export default new AuthService();
