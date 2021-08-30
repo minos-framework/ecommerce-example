b46ec2d1238c6fb8b7668e1b1ae0d7a951ca0b3a
Alberto Amigo Alonso <albamigh@gmail.com>
2021-08-30 11:41:29 +0200

    - Added ModelRef.
    issue #252

--- microservices/authentication/src/aggregates.py
+++ microservices/authentication/src/aggregates.py
@@ -4,10 +4,17 @@ from __future__ import (
 
 from minos.common import (
     Aggregate,
+    AggregateRef,
+    ModelRef
 )
+
+
+class User(AggregateRef):
+    pass
 
 
 class Credential(Aggregate):
     username: str
     password: str
     active: bool
+    user = ModelRef[User]
