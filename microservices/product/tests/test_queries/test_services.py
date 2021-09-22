87e0c2043dc823886f63cf7d4f2e77829e6fd662
Sergio García Prado <sergio.garcia@clariteia.com>
2021-09-22 16:42:13 +0200

    ISSUE #287 * Clean `product` microservice.

--- microservices/product/tests/test_queries/test_services.py
+++ microservices/product/tests/test_queries/test_services.py
@@ -4,92 +4,18 @@ from __future__ import (
 
 import sys
 import unittest
-from pathlib import (
-    Path,
-)
-from typing import (
-    NoReturn,
-    Optional,
-)
-from uuid import (
-    UUID,
-    uuid4,
-)
-
-from cached_property import (
-    cached_property,
-)
-from minos.common import (
-    CommandReply,
-    DependencyInjector,
-    InMemoryRepository,
-    InMemorySnapshot,
-    MinosBroker,
-    MinosConfig,
-    MinosSagaManager,
-    Model,
-)
-from minos.networks import (
-    Request,
-)
 
 from src import (
     ProductQueryService,
 )
-
-
-class _FakeRequest(Request):
-    """For testing purposes"""
-
-    def __init__(self, content):
-        super().__init__()
-        self._content = content
-
-    @cached_property
-    def user(self) -> Optional[UUID]:
-        """For testing purposes"""
-        return uuid4()
-
-    async def content(self, **kwargs):
-        """For testing purposes"""
-        return self._content
-
-    def __eq__(self, other: _FakeRequest) -> bool:
-        return self._content == other._content and self.user == other.user
-
-    def __repr__(self) -> str:
-        return str()
-
-
-class _FakeBroker(MinosBroker):
-    """For testing purposes."""
-
-    async def send(self, items: list[Model], **kwargs) -> NoReturn:
-        """For testing purposes."""
-
-
-class _FakeSagaManager(MinosSagaManager):
-    """For testing purposes."""
-
-    async def _run_new(self, name: str, **kwargs) -> UUID:
-        """For testing purposes."""
-
-    async def _load_and_run(self, reply: CommandReply, **kwargs) -> UUID:
-        """For testing purposes."""
+from tests.utils import (
+    build_dependency_injector,
+)
 
 
 class TestProductQueryService(unittest.IsolatedAsyncioTestCase):
-    CONFIG_FILE_PATH = Path(__file__).parents[2] / "config.yml"
-
     async def asyncSetUp(self) -> None:
-        self.config = MinosConfig(self.CONFIG_FILE_PATH)
-        self.injector = DependencyInjector(
-            self.config,
-            saga_manager=_FakeSagaManager,
-            event_broker=_FakeBroker,
-            repository=InMemoryRepository,
-            snapshot=InMemorySnapshot,
-        )
+        self.injector = build_dependency_injector()
         await self.injector.wire(modules=[sys.modules[__name__]])
 
         self.service = ProductQueryService()
