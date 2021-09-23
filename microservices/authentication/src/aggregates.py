c9385b665756b4a43d62423a4b5b0c971e6df38b
Sergio García Prado <sergio.garcia@clariteia.com>
2021-09-23 08:18:46 +0200

    ISSUE #268 * Add validation to check if username already exists.

--- microservices/authentication/src/aggregates.py
+++ microservices/authentication/src/aggregates.py
@@ -4,10 +4,29 @@ from __future__ import (
 
 from minos.common import (
     Aggregate,
+    Condition,
 )
 
 
 class Credentials(Aggregate):
+    """Credentials Aggregate class.
+
+    The purpose of this aggregate is to store the needed information to be authenticated.
+    """
+
     username: str
     password: str
     active: bool
+
+    @classmethod
+    async def exists_username(cls, username: str) -> bool:
+        """Check if username already exists.
+
+        :param username: The username to be checked.
+        :return: ``True`` if the username exists or ``False`` otherwise.
+        """
+        try:
+            await cls.find(Condition.EQUAL("username", username)).__anext__()
+            return True
+        except StopAsyncIteration:
+            return False
