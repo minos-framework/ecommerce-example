eafdd0a2051569103c5d96adad2110d600ecbd78
Sergio García Prado <sergio.garcia@clariteia.com>
2021-10-07 09:40:25 +0200

    ISSUE #252 * Fix syntax.

--- microservices/authentication/src/commands/services.py
+++ microservices/authentication/src/commands/services.py
@@ -1,6 +1,11 @@
+import logging
+
 import jwt
 from jwt.exceptions import (
     InvalidSignatureError,
+)
+from minos.common import (
+    Condition,
 )
 from minos.cqrs import (
     CommandService,
@@ -26,6 +31,8 @@ from ..jwt_env import (
 from .sagas import (
     CREATE_CREDENTIALS_SAGA,
 )
+
+logger = logging.getLogger(__name__)
 
 
 class CredentialsCommandService(CommandService):
@@ -73,8 +80,35 @@ class CredentialsCommandService(CommandService):
 
         await credentials.delete()
 
+    @enroute.broker.event("CustomerDeleted")
+    async def user_deleted(self, request: Request) -> None:
+        """Delete the associated credentials to the already deleted user.
+
+        :param request: A ``Request`` containing a ``AggregateDiff`` instance.
+        :return: This method does not return anything.
+        """
+
+        diff = await request.content()
+        user = diff.uuid
+
+        entries = {credentials async for credentials in Credentials.find(Condition.EQUAL("user", user))}
+
+        if len(entries) == 0:
+            return
+
+        if len(entries) > 1:
+            logger.warning(f"The user identified by {user!r} had multiple associated credentials")
+
+        for credentials in entries:
+            await credentials.delete()
+
     @enroute.rest.command("/token", "POST")
     async def validate_jwt(self, request: RestRequest) -> Response:
+        """Validate if the given ``jwt`` token is valid.
+
+        :param request: A ``RestRequest`` containing the token in headers.
+        :return: The response containing the payload if everything is fine or an exception otherwise.
+        """
         auth_type, jwt_token = request.raw_request.headers["Authorization"].split()
 
         if auth_type == "Bearer":
